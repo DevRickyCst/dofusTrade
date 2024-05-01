18ad99b40386002493ae2386a8393e4f57a3cf1d
DevRickyCst <dev.creusot.aym@gmail.com>
2024-05-01 17:51:01 +0200

    create items models

--- itemViewer/models.py
+++ itemViewer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+ 
+class item(models.Model):
+    id = models.IntegerField(primary_key=True),
+    categorie = models.CharField(max_length=20),
+    name = models.CharField(max_length=100),
+    type = models.CharField(max_length=30),
+    level = models.IntegerField(),
+    image_icon = models.CharField(max_length=100),
+    image_sd = models.CharField(max_length=100),
+    image_hq = models.CharField(max_length=100),
+    image_hd = models.CharField(max_length=100),
+    description = models.CharField(max_length=1000),
+    effects = models.JSONField(),
+    recipe = models.JSONField(),
+    condition_tree = models.JSONField(),
