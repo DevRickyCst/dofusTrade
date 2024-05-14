8db12e159e377778f8ed07ba3ab939e0d3e4e77d
ck <none>
2024-05-14 08:29:21 +0200

    added __str__() on certain models

--- characterManager/models.py
+++ characterManager/models.py
@@ -7,6 +7,9 @@ class CharacterClass(models.Model):
     name = models.CharField(max_length=100, default="random")
     logo_url = models.CharField(max_length=100, default="random")
 
+    def __str__(self) :
+        return "id: " + self.id.__str__() + ", name : " + self.name
+
 
 class Character(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -15,4 +18,7 @@ class Character(models.Model):
     server = models.CharField(max_length=100)
     character_class = models.ForeignKey(CharacterClass, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)  # Foreign key to User model
+
+    def __str__(self) :
+        return "id: " + self.id.__str__() + ", name : " + self.name
 
