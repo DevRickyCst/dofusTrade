03fafbe5a583d32a821979a516b432ed2dd59773
ck <none>
2024-05-16 00:27:10 +0200

    Refactor insertion in db

--- src/management/commands/db-management.py
+++ src/management/commands/db-management.py
@@ -12,8 +12,6 @@ from itemViewer.models import Item
 from src.management.commands.__ApiTypeEnum import ApiTypeEnum as ApiTypeEnum
 
 
-# Defining the host is optional and defaults to https://api.dofusdu.de
-# See configuration.py for a list of all supported configuration parameters.
 class Command(BaseCommand):
     help = "Fill Bdd with consumables"
     added_items_count = 0
@@ -23,20 +21,25 @@ class Command(BaseCommand):
         print()
 
     def handle(self, *args, **options):
-
+        # Defining the host is optional and defaults to https://api.dofusdu.de
         configuration = dofusdude.Configuration(host="https://api.dofusdu.de")
 
         self.clean_db()
 
+        self.call_consumable_api(configuration)
+
         self.call_equipment_api(configuration)
 
-        self.call_consumable_api(configuration)
+        print(
+            "Added a Total of "
+            + self.added_items_count.__str__()
+            + " items to db"
+        )
 
     # Calls the consumables API, fetch all consumables and add them to bd with field categorie set as "consumable"
     def call_consumable_api(self, configuration):
         added_consumables = 0
         try:
-            # Consumables endpoint
             json_api_response = self.get_API_response(
                 configuration, ApiTypeEnum.CONSUMABLE
             ).to_json()
@@ -47,16 +50,7 @@ class Command(BaseCommand):
                 + " consumables\n"
             )
             for item in json_consumable["items"]:
-                i = Item(
-                    ankama_id=item["ankama_id"],
-                    name=item["name"],
-                    type=item["type"]["name"],
-                    level=item["level"],
-                    image_urls=item["image_urls"],
-                )
-                i.categorie = "consumables"
-                i.save()
-                self.added_items_count += 1
+                self.insert_in_Item_Table(item)
                 added_consumables += 1
             print(
                 "Added " + added_consumables.__str__() + " consumables to db"
@@ -67,31 +61,23 @@ class Command(BaseCommand):
                 % e
             )
 
-    # Calls the consumables API, fetch all equipments and add them to bd with field categorie set as "equipment"
+    # Calls the equipment API, fetch all equipments and add them to bd with field categorie set as "equipment"
     def call_equipment_api(self, configuration):
         added_equipment = 0
         try:
-            # List Equipment
             api_response = self.get_API_response(
                 configuration, ApiTypeEnum.EQUIPMENT
             )
-            print("The response of EquipmentApi->get_items_equipment_list:\n")
-            # json_response = json.loads("{" + api_response.__str__() + "}")
+            print(
+                "The response of EquipmentApi->get_items_equipment_list contains "
+                + len(api_response.items).__str__()
+                + " equipments"
+            )
             for item in api_response.items:
-                # print(item.to_json())
                 json_item = json.loads(item.to_json())
-                i = Item(
-                    ankama_id=json_item["ankama_id"],
-                    name=json_item["name"],
-                    type=json_item["type"]["name"],
-                    level=json_item["level"],
-                    image_urls=json_item["image_urls"],
-                )
-                i.categorie = "equipment"
-                i.save()
+                self.insert_in_Item_Table(json_item)
                 added_equipment += 1
             print(added_equipment)
-            # print(json.dumps(json_response, indent=2))
         except Exception as e:
             print(
                 "Exception when calling EquipmentApi->get_items_equipment_list: %s\n"
@@ -112,14 +98,12 @@ class Command(BaseCommand):
     # Create the api instance and return the response from the correct API
     def get_API_response(self, configuration, api_type):
         with dofusdude.ApiClient(configuration) as api_client:
-            # Create an instance of the API class
-            language = "fr"  # str | a valid language code
-            game = "dofus2"  # str |
-            sort_level = "asc"  # str | sort the resulting list by level, default unsorted (optional)
-            page_size = (
-                -1
-            )  # int | size of the results from the list. -1 disables pagination and gets all in one response. (optional)
-            page_number = 1  # int | page number based on the current page[size]. So you could get page 1 with 8 entrys and page 2 would have entries 8 to 16. (optional)
+            # Common parameters to all API
+            language = "fr"
+            game = "dofus2"
+            sort_level = "asc"
+            page_size = -1
+            page_number = 1
 
             if api_type == ApiTypeEnum.EQUIPMENT:
                 api_instance = dofusdude.EquipmentApi(api_client)
@@ -145,31 +129,15 @@ class Command(BaseCommand):
                     page_size=page_size,
                     page_number=page_number,
                 )
-
-    """
-# Enter a context with an instance of the API client
-with dofusdude.ApiClient(configuration) as api_client:
-# Create an instance of the API class
-api_instance = dofusdude.AlmanaxApi(api_client)
-language = 'fr' # str | code
-var_date = date.today() # date | yyyy-mm-dd
-
-try:
-    # Single Almanax Date
-    api_response = api_instance.get_almanax_date(language, var_date)
-    print("The response of AlmanaxApi->get_almanax_date:\n")
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling AlmanaxApi->get_almanax_date: %s\n" % e)
-"""
-
-
-"""payload = {'page_size': '10', 'filter_max_level' : 2}
-#r = requests.get('https://api.dofusdu.de/dofus2/fr/items/consumables/', params=payload)
-r = requests.get('https://api.dofusdu.de/dofus2/fr/items/consumables?page-number=1&page-size=16')
-
-print(r.status_code)
-jsonResponse = json.loads(r.content)
-jsonReadable = json.dumps(jsonResponse, indent=2)
-print(len(jsonResponse["items"][0]))
-print(jsonReadable)"""
+            
+    def insert_in_Item_Table(self, item):
+        i = Item(
+                ankama_id=item["ankama_id"],
+                name=item["name"],
+                type=item["type"]["name"],
+                level=item["level"],
+                image_urls=item["image_urls"],
+            )
+        i.categorie = "consumables"
+        i.save()
+        self.added_items_count += 1
